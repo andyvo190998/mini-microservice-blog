ef82fdeea94509cadb935f3d9eb8937eb260ba2c
AndyVo <andy.vo@kiutra.com>
2024-04-15 16:11:56 +0200

    first commit

---
+++ comments/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+const commentsByPostId = {};
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello world!');
+});
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+
+app.post('/posts/:id/comments', (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentsByPostId[req.params.id] || [];
+    comments.push({ id: id, content: content });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(200).send(comments);
+});
+
+
+app.listen(4001, () => console.log('comments service is listening on 4001'));
