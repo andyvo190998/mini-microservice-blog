1185a073da3e1a581409be0f0215196e3cd74796
Andy Vo <andyvo190998@gmail.com>
2024-04-18 15:40:24 +0200

    update frontend to moderation

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -5,9 +5,18 @@ const CommentList = ({ comments }) => {
     const renderComment = Object.values(comments) || []
     return (
         <ul>
-            {renderComment.map(comment => (
-                <li key={comment.content}>{comment.content}</li>
-            ))}
+            {renderComment.map(comment => {
+                let content
+                if (comment.status === 'approved') {
+                    content = comment.content
+                } else if (comment.status === 'pending') {
+                    content = 'This comment is awaiting moderation'
+                } else if (comment.status === 'rejected') {
+                    content = 'This comment has been rejected'
+                }
+
+                return <li key={comment.content}>{content}</li>
+            })}
         </ul>
     )
 }
